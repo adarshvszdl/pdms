e0428082bf49510b36f104713beab0527e127717
Adarsh V S <adarsh@zdistancelab.com>
2024-10-12 16:33:21 +0530

    mn

--- PDMS/src/config/config.js
+++ PDMS/src/config/config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://ec2-3-25-65-226.ap-southeast-2.compute.amazonaws.com:5000"
 export const BASE_URL = "http://localhost:5000"
+// export const BASE_URL = "http://ec2-3-25-67-19.ap-southeast-2.compute.amazonaws.com:5000"
